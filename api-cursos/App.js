de3afab01d389361a345045383c900ca6034ea57
Airton Marinho <93198566+AirtonM@users.noreply.github.com>
2025-08-18 16:43:09 -0300

    Construindo um CRUD Full-Stack
    Aula 08 - Construindo um CRUD Full-Stack

--- api-cursos/App.js
+++ api-cursos/App.js
@@ -1,21 +1,23 @@
-// Arquivo: app-cursos/App.js
-
+// app-cursos/App.js
 import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
-import ListaCursos from './src/components/ListaCursos'; // 1. IMPORTAMOS NOSSO COMPONENTE
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Renomeie 'ListaCursos.js' para 'ListaCursosScreen.js' e mova para 'src/screens'
+import ListaCursosScreen from './src/screens/ListaCursosScreen';
+import AddCursoScreen from './src/screens/AddCursoScreen';
+import EditCursoScreen from './src/screens/EditCursoScreen'; // 1. Importar a nova tela
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <ListaCursos /> {/* 2. USAMOS NOSSO COMPONENTE AQUI */}
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ListaCursos">
+        <Stack.Screen name="ListaCursos" component={ListaCursosScreen} options={{ title: 'Cursos Disponíveis' }} />
+        <Stack.Screen name="AddCurso" component={AddCursoScreen} options={{ title: 'Adicionar Novo Curso' }} />
+        <Stack.Screen name="EditCurso" component={EditCursoScreen} options={{ title: 'Editar Curso' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-});
